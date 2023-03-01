a62dbf8daa17d118f4791b7f7a9a5a4662a67248
Thilo Balke <thilo.balke@gmail.com>
2023-03-01 13:11:01 -0700

    save

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,21 @@
 # Configuration file for the Sphinx documentation builder.
 
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import sphinx_rtd_theme
+
+if os.environ.get('TRINIDI_BUILD_DOCS') =='true':
+    sys.path.insert(0, os.path.abspath('../..'))
+
+
+
 # -- Project information
 
 project = 'trinidi'
