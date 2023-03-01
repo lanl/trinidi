8a65e467ded57e77d7216ea6f3842cca5b2004af
Thilo Balke <thilo.balke@gmail.com>
2023-03-01 14:05:13 -0700

    save

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@ rootpath = os.path.join(confpath, "..", "..")
 sys.path.append(rootpath)
 
 
+from docutil import insert_inheritance_diagram, package_classes
+
 # from trinidi._version import package_version
 def package_version(): # update this ToDo
     return '0.0.1'
