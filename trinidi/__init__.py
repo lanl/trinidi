9ee5b077f61063131d17f0da3968877d559ba711
Thilo Balke <thilo.balke@gmail.com>
2023-03-01 11:40:26 -0700

    init

---
+++ trinidi/__init__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2023 by Thilo Balke
+# All rights reserved. BSD 3-clause License.
+# This file is part of the TRINIDI package. Details of the copyright and
+# user license can be found in the 'LICENSE' file distributed with the
+# package.
+
+"""Time-of-Flight Resonance Imaging with Neutrons for Isotopic Density Inference (TRINIDI)
+is a Python package estimating isotopic densities using neutron transmission resonance images. 
+"""
+
+__version__ = "0.0.1"
+
+import sys
+
+from . import _python37  # python 3.7 compatibility
+
+
+__all__ = []
+
+# Imported items in __all__ appear to originate in top-level functional module
+for name in __all__:
+    getattr(sys.modules[__name__], name).__module__ = __name__
