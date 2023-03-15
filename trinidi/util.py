77163fa0699d6989f4011cdf22835381c5ed0e0b
Thilo Balke <thilo.balke@gmail.com>
2023-03-15 12:49:08 -0600

    typos

--- trinidi/util.py
+++ trinidi/util.py
@@ -2,26 +2,11 @@
 
 import numpy as np
 
-MASS_OF_NEUTRON = 939.56542052 * 1e6 / (299792458) ** 2  # mass neutron [eV s²/m²]
-
-
-def fibs(N):
-    r"""Generate a list of the first N Fibonacci numbers.
-
-    The Fibonacci sequence is defined as
-
-    .. math::
-        f(n+2) = f(n+1) + f(n)\;,
-
-    where :math:`f(1) = f(2) = 0`.
-    """
-    fib = lambda n: n if n < 2 else fib(n - 1) + fib(n - 2)
-
-    return np.array([fib(i) for i in range(N)])
+MASS_OF_NEUTRON = 939.56542052 * 1e6 / (299792458) ** 2  # [eV s²/m²]
 
 
 def time2energy(flight_path_length, time):
-    r"""Convert time-of-flight to energy of the neutron
+    r"""Convert time-of-flight to energy of the neutron.
 
     .. math::
         E = \frac{1}{2} m \left( \frac{L}{t} \right) \; ,
@@ -30,25 +15,24 @@ def time2energy(flight_path_length, time):
     the time-of-flight of the neutron, and :math:`L` is the flight path
     lenghth.
 
-
     Args:
         flight_path_length: flight path length in :math:`\mathrm{m}`.
-        time: Time-of-flight in :math:`\mathrm{μs}`
+        time: Time-of-flight in :math:`\mathrm{μs}`.
 
     Returns:
         Energy of the neutron in :math:`\mathrm{eV}`.
     """
     m = MASS_OF_NEUTRON  # [eV s²/m²]
-    L = flight_path_length  # meters
-    t = time / 1e6  # seconds
+    L = flight_path_length  # m
+    t = time / 1e6  # s
     return m / 2 * (L / t) ** 2  # eV
 
 
 def energy2time(flight_path_length, energy):
-    r"""Convert energy of the neutron to time-of-flight
+    r"""Convert energy to time-of-flight of the neutron.
 
     .. math::
-        E = \frac{1}{2} m \left( \frac{L}{t} \right) \; ,
+        t = L \sqrt{ \frac{m}{2 E} }
 
     where :math:`E` is the energy, :math:`m` is the mass, :math:`t` is
     the time-of-flight of the neutron, and :math:`L` is the flight path
@@ -62,8 +46,8 @@ def energy2time(flight_path_length, energy):
         Time-of-flight in :math:`\mathrm{μs}`.
 
     """
-    L = flight_path_length
-    m = MASS_OF_NEUTRON  # [eV s²/m²]
+    L = flight_path_length  # m
+    m = MASS_OF_NEUTRON  # eV s²/m²
     E = energy  # eV
-    t = L * np.sqrt(m / 2 / E)  # seconds
-    return t * 1e6  # micro seconds
+    t = L * np.sqrt(m / 2 / E)  # s
+    return t * 1e6  # μs
