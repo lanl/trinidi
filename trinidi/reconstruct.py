63f4054d04b585b58d8501cdeb8b4899ccba8340
Thilo Balke <thilo.balke@gmail.com>
2023-03-24 21:03:50 -0600

    save

--- trinidi/reconstruct.py
+++ trinidi/reconstruct.py
@@ -1,9 +1,24 @@
 """Some reconstruction functions and classes."""
 
-from jax.config import config
+import jax
+import jax.config
+import matplotlib.pyplot as plt
+import numpy as np
+import scico.numpy as snp
+from scico.functional import (
+    Functional,
+    NonNegativeIndicator,
+    SeparableFunctional,
+    ZeroFunctional,
+)
+from scico.loss import SquaredL2Loss
 from scico.numpy import BlockArray
+from scico.operator import Operator
+from scico.optimize.pgm import AcceleratedPGM, RobustLineSearchStepSize
 
-config.update("jax_enable_x64", True)
+from trinidi import cross_section, resolution, util
+
+jax.config.update("jax_enable_x64", True)
 
 
 class ProjectionRegion:
@@ -81,10 +96,134 @@ def plot_densities(fig, ax, Z, isotopes):
     return fig, ax
 
 
-import matplotlib.pyplot as plt
-import numpy as np
+class FunctionalSum(Functional):
+    r"""A sum of two functionals."""
 
-from trinidi import cross_section, resolution, util
+    def __repr__(self):
+        return (
+            "Sum of functionals of types "
+            + str(type(self.functional1))
+            + " and "
+            + str(type(self.functional2))
+        )
+
+    def __init__(self, functional1: Functional, functional2: Functional):
+        self.functional1 = functional1
+        self.functional2 = functional2
+        self.has_eval = functional1.has_eval and functional2.has_eval
+        self.has_prox = False
+        super().__init__()
+
+    def __call__(self, x) -> float:
+        return self.functional1(x) + self.functional2(x)
+
+
+class Forward_zα1α2θ(Operator):
+    r"""
+    Pseudo forward operator
+    f(z,ε,μ,θ) = ε[ {c_o - b(θ)} * q(z) +  μ b(θ)]
+
+    c_o = (ω' C_o) / (ω'v)
+    q(z) = exp(-z D) R
+    b(θ) = exp(θ P)
+
+    This gets fitted to
+    c_s = (ω' C_s) / (ω'v)
+    """
+
+    def __init__(self, input_shape, y_o, D, R, P, jit: bool = True):
+        self.y_o = y_o
+        self.D = jax.device_put(D.values)
+        self.R = R.single
+        self.P = jax.device_put(P)
+
+        super().__init__(
+            input_shape=input_shape,
+            jit=jit,
+        )
+
+    def _eval(self, zα1α2θ):
+        z = zα1α2θ[0]
+        α_1 = zα1α2θ[1]
+        α_2 = zα1α2θ[2]
+        θ = zα1α2θ[3]
+        b = (snp.exp(θ.T @ self.P)).T
+        q = self.R(snp.exp(-z.T @ self.D)).T
+        y_o = self.y_o
+        return α_1 * ((y_o - b) * q + α_2 * b)
+
+
+class Forward_0α1α2θ(Operator):
+    r"""
+    Pseudo forward operator
+    f(0,ε,μ,θ) = ε[ {c_o - b(θ)} * q(z) +  μ b(θ)]
+               = ε[ c_o +  (μ-1) b(θ)]
+
+    c_o = (ω' C_o) / (ω'v)
+    q(0) = exp(-z D) R = 1
+    b(θ) = exp(θ P)
+
+    This gets fitted to
+    c_s = (ω' C_s) / (ω'v)
+
+    """
+
+    def __init__(self, input_shape, y_o, P, jit: bool = True):
+        self.y_o = y_o
+        self.P = jax.device_put(P)
+
+        super().__init__(
+            input_shape=input_shape,
+            jit=jit,
+        )
+
+    def _eval(self, zα1α2θ):
+        # z = zα1α2θ[0] # not used
+        α_1 = zα1α2θ[1]
+        α_2 = zα1α2θ[2]
+        θ = zα1α2θ[3]
+        b = (snp.exp(θ.T @ self.P)).T
+        y_o = self.y_o
+        return α_1 * (y_o + (α_2 - 1) * b)
+
+
+import copy
+
+
+class Preconditioner:
+    """(Z) (D)  = (ZC) (C_inverse D)
+    original   conditioned
+    where (C_inverse D) has unit norm rows
+
+    C = np.diag(np.diag(np.matmul(D, D.T)))**0.5
+    """
+
+    def __init__(self, D):
+        self._C = np.diag(np.diag(np.matmul(D.values, D.values.T))) ** 0.5
+        self._C_inverse = np.linalg.inv(self._C)
+        self.D_conditioned = copy.deepcopy(D)
+        self.D_conditioned.values = self._C_inverse @ D.values
+
+    def condition_Z(self, Z):
+        r"""Condition Z"""
+        return Z @ self._C
+
+    def uncondition_Z(self, Z_conditioned):
+        r"""Uncondition Z"""
+        return Z_conditioned @ self._C_inverse
+
+    def condition_zα1α2θ(self, zα1α2θ):
+        r"""Condition zα1α2θ"""
+        zα1α2θ_conditioned = zα1α2θ.copy()
+        zα1α2θ_conditioned[0] = self.condition_Z(zα1α2θ[0].T).T
+        return zα1α2θ_conditioned
+
+    def uncondition_zα1α2θ(self, zα1α2θ_conditioned):
+        r"""Uncondition zα1α2θ"""
+        zα1α2θ = zα1α2θ_conditioned.copy()
+        zα1α2θ[0] = self.uncondition_Z(zα1α2θ_conditioned[0].T).T
+        return zα1α2θ
+
 
 Δt = 0.90
 t_A = np.arange(72, 400, Δt)
@@ -160,106 +299,6 @@ fig.suptitle("")
 # plt.show()
 
 
-import jax
-import scico.numpy as snp
-from scico.functional import Functional
-from scico.loss import SquaredL2Loss
-from scico.operator import Operator
-
-
-class FunctionalSum(Functional):
-    r"""A sum of two functionals."""
-
-    def __repr__(self):
-        return (
-            "Sum of functionals of types "
-            + str(type(self.functional1))
-            + " and "
-            + str(type(self.functional2))
-        )
-
-    def __init__(self, functional1: Functional, functional2: Functional):
-        self.functional1 = functional1
-        self.functional2 = functional2
-        self.has_eval = functional1.has_eval and functional2.has_eval
-        self.has_prox = False
-        super().__init__()
-
-    def __call__(self, x) -> float:
-        return self.functional1(x) + self.functional2(x)
-
-
-class Forward_zα1α2θ(Operator):
-    r"""
-    Pseudo forward operator
-    f(z,ε,μ,θ) = ε[ {c_o - b(θ)} * q(z) +  μ b(θ)]
-
-    c_o = (ω' C_o) / (ω'v)
-    q(z) = exp(-z D) R
-    b(θ) = exp(θ P)
-
-    This gets fitted to
-    c_s = (ω' C_s) / (ω'v)
-
-
-    """
-
-    def __init__(self, input_shape, y_o, D, R, P, jit: bool = True):
-        self.y_o = y_o
-        self.D = jax.device_put(D.values)
-        self.R = R.single
-        self.P = jax.device_put(P)
-
-        super().__init__(
-            input_shape=input_shape,
-            jit=jit,
-        )
-
-    def _eval(self, zα1α2θ):
-        z = zα1α2θ[0]
-        α_1 = zα1α2θ[1]
-        α_2 = zα1α2θ[2]
-        θ = zα1α2θ[3]
-        b = (snp.exp(θ.T @ self.P)).T
-        q = self.R(snp.exp(-z.T @ self.D)).T
-        y_o = self.y_o
-        return α_1 * ((y_o - b) * q + α_2 * b)
-
-
-class Forward_0α1α2θ(Operator):
-    r"""
-    Pseudo forward operator
-    f(0,ε,μ,θ) = ε[ {c_o - b(θ)} * q(z) +  μ b(θ)]
-               = ε[ c_o +  (μ-1) b(θ)]
-
-    c_o = (ω' C_o) / (ω'v)
-    q(0) = exp(-z D) R = 1
-    b(θ) = exp(θ P)
-
-    This gets fitted to
-    c_s = (ω' C_s) / (ω'v)
-
-    """
-
-    def __init__(self, input_shape, y_o, P, jit: bool = True):
-        self.y_o = y_o
-        self.P = jax.device_put(P)
-
-        super().__init__(
-            input_shape=input_shape,
-            jit=jit,
-        )
-
-    def _eval(self, zα1α2θ):
-        # z = zα1α2θ[0] # not used
-        α_1 = zα1α2θ[1]
-        α_2 = zα1α2θ[2]
-        θ = zα1α2θ[3]
-        b = (snp.exp(θ.T @ self.P)).T
-        y_o = self.y_o
-        return α_1 * (y_o + (α_2 - 1) * b)
-
-
 class Parameters:
     r"""Parameter class for nuisance parameters.
     :code:`projection_shape` is the shape of the detector so usually this will
@@ -275,7 +314,7 @@ class Parameters:
     """
 
     def __init__(
-        self, Y_o, Y_s, R, D, Ω_z, Ω_0=None, N_b=5, β=1.0, optimization_params=None
+        self, Y_o, Y_s, R, D, Ω_z, Ω_0=None, N_b=5, β=1.0, non_negative_z=False
     ):
         r"""
         Args:
@@ -297,6 +336,9 @@ class Parameters:
         self.t_A = self.R.t_A
         self.t_F = self.R.t_F
         self.D = D
+        self._pc = Preconditioner(D)
+        self._D_conditioned = self._pc.D_conditioned
+
         self.P = util.background_basis(N_b, self.t_A.size)
 
         self.Y_o = Y_o
@@ -320,23 +362,62 @@ class Parameters:
         self.zα1α2θ_init = self._initialize(
             self.Ω_0, self.Y_o, self.Y_s, self.y_sz, self.y_o, self.D, self.R, self.P
         )
+        self._zα1α2θ_init_conditioned = self._pc.condition_zα1α2θ(self.zα1α2θ_init)
 
         # --- When not Ω_0:
-        self.Forward_z = Forward_zα1α2θ(
+        self._forward_z_conditioned = Forward_zα1α2θ(
+            self.zα1α2θ_init.shape, self.y_o, self._D_conditioned, self.R, self.P
+        )
+        self.forward_z = Forward_zα1α2θ(
             self.zα1α2θ_init.shape, self.y_o, self.D, self.R, self.P
         )
-        fz = SquaredL2Loss(y=jax.device_put(self.y_sz), A=self.Forward_z)
+
+        fz = SquaredL2Loss(y=jax.device_put(self.y_sz), A=self._forward_z_conditioned)
         fz.is_smooth = True
 
         if self.Ω_0:
-            self.Forward_0 = Forward_0α1α2θ(self.zα1α2θ_init.shape, self.y_o, self.P)
+            # f = || y_sz - f(z, α_1, α_2, θ) ||^2  +  || y_s0 - f(0, α_1, α_2, θ) ||^2
+            self.forward_0 = Forward_0α1α2θ(self.zα1α2θ_init.shape, self.y_o, self.P)
             self.y_s0 = (self.Ω_0.average(Y_s) / self.Ω_0.average(self.v)).T
-            f0 = SquaredL2Loss(y=jax.device_put(self.y_s0), A=self.Forward_0)
+            f0 = SquaredL2Loss(y=jax.device_put(self.y_s0), A=self.forward_0)
             f0.is_smooth = True
-            self.f = FunctionalSum(fz, f0)
+            f = FunctionalSum(fz, f0)
 
         else:
-            self.f = fz
+            # f = || y_sz - f(z, α_1, α_2, θ) ||^2
+            self.forward_0 = None
+            self.y_s0 = None
+            f = fz
+
+        # --- g constraints
+        if non_negative_z:
+            gz = NonNegativeIndicator()
+        else:
+            gz = ZeroFunctional()
+
+        gα1 = NonNegativeIndicator()
+        gα2 = NonNegativeIndicator()
+        gθ = ZeroFunctional()
+
+        g = SeparableFunctional([gz, gα1, gα2, gθ])
+
+        step_size = RobustLineSearchStepSize()
+        L0 = 1e-5
+        self.apgm = AcceleratedPGM(
+            f=f,
+            g=g,
+            L0=L0,
+            x0=self._zα1α2θ_init_conditioned,
+            step_size=step_size,
+            itstat_options={"display": True, "period": 10},
+        )
+
+    def solve(self, iterations=100):
+        r"""Find parameters."""
+        self.apgm.maxiter = iterations
+        zα1α2θ_conditioned = self.apgm.solve()
+        self.zα1α2θ = self._pc.uncondition_zα1α2θ(zα1α2θ_conditioned)
+        return self.zα1α2θ
 
     def _initialize(self, Ω_0, Y_o, Y_s, y_sz, y_o, D, R, P):
         r"""Initialize parameters."""
@@ -428,6 +509,8 @@ class Parameters:
 
 
 par = Parameters(Y_o, Y_s, R, D, Ω_z, Ω_0=Ω_0)
+
+zα1α2θ = par.solve(iterations=50000)
 
 
 par.plot_regions()
