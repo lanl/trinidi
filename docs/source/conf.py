ef3429de723072d77630df46eba99b77a4dcec48
Thilo Balke <thilo.balke@gmail.com>
2023-04-10 19:58:27 -0600

    ipython extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,7 +84,8 @@ extensions = [
     "sphinx.ext.inheritance_diagram",
     "sphinx.ext.mathjax",
     "sphinx.ext.todo",
-    "nbsphinx",
+    "nbsphinx" "IPython.sphinxext.ipython_console_highlighting",
+    "IPython.sphinxext.ipython_directive",
 ]
 
 bibtex_bibfiles = ["references.bib"]
