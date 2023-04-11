13c81bbd9fad1e6ca73e1b84221ac1021c582eea
Thilo Balke <thilo.balke@gmail.com>
2023-04-10 19:25:50 -0600

    fac theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -187,8 +187,8 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
-# html_theme = "faculty-sphinx-theme"
+# html_theme = "sphinx_rtd_theme"
+html_theme = "faculty-sphinx-theme"
 # html_theme = 'karma_sphinx_theme'
 
 html_theme_options = {
