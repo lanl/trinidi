9c6f71cf0393eb696b9eadd59f249c1770b50bd7
Thilo Balke <thilo.balke@gmail.com>
2023-04-12 11:35:05 -0600

    save

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,7 +86,6 @@ extensions = [
     "sphinx.ext.todo",
     "nbsphinx",
     "IPython.sphinxext.ipython_console_highlighting",
-    # "IPython.sphinxext.ipython_directive",
 ]
 
 bibtex_bibfiles = ["references.bib"]
